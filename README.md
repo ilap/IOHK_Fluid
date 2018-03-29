5a28e4376e41551b278daa1503873197005b2dae
Pal Dorogi <Pal.Dorogi@gmail.com>
2018-03-29 13:19:34 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# GPU Fluid Experiments
+
+Credits:
+Haxiomic: https://github.com/haxiomic/GPU-Fluid-Experiments
+IOHK: N/A
+
+
+# Install
+
+ $ npm install connect serve-static
+ $ node server.js
+Server running on 8080...
+
+Open browser 
+
---
+++ server.js
@@ -0,0 +1,6 @@
+// npm install connect serve-static
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
